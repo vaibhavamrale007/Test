8f65fc0a6fd69a2e730a68489a76ac85788c38a7
vaibhavamrale007 <vaibhavamrale720@gmail.com>
2025-07-29 12:39:55 +0530

    one

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        if (a < c) {
+            System.out.println("c is grater than a");
+        }
+
     }
+
 }
---
+++ Oddeven.java
@@ -0,0 +1,15 @@
+public class Oddeven {
+    public static void main (String []args){
+        int a=10;
+        int b=20;
+        int c=30;
+        if(a<c){
+            System.out.println("c is grater than a");
+        }
+
+    }
+
+
+}
+
+
